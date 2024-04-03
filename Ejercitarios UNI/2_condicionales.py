ce78083b2e0a57787355c3ce03c91b9ab6311997
Samuel Cáceres <144251952+samuelcaceres@users.noreply.github.com>
2024-04-02 20:14:48 -0400

    Update 2_condicionales.py

---
+++ Ejercitarios UNI/2_condicionales.py
@@ -0,0 +1,34 @@
+#1
+"""
+num_uno = int(input('Ingrese el primer numero: '))
+num_dos = int(input('Ingrese el segundo numero: '))
+
+if num_uno == num_dos:
+    print('Los numeros son iguales.')
+else:
+    print('Los numero sosn diferentes.')
+
+if num_uno > num_dos:
+    print('El primer numero es mayor.')
+else:
+    print('El segundo numero es mayor o igual que el primero.')
+"""
+
+#2
+"""
+num_uno = int(input('Ingrese el primer numero: '))
+num_dos = int(input('Ingrese el segundo numero: '))
+num_tres = int(input('Ingrese el tercer numero: '))
+
+if num_uno == num_dos == num_tres:
+    print('Los numeros son iguales.')
+else:
+    print('Los numeros son distintos.')
+
+
+if num_uno > num_dos and num_uno > num_tres:
+    print('El primer numero es el mayor.')
+elif num_dos > num_uno and num_dos > num_tres:
+    print('El segundo numero es el mayor.')
+else:
+    print('El tercer numero es el mayor.')"""
